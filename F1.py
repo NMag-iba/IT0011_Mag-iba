bfe2c396c820ca4254e4e24b8a7432bdfb4fb2d3
NMag-iba <nhicamagiba2704@gmail.com>
2025-01-20 10:40:36 +0800

    01/20/2025 - 10:40 - F1

---
+++ F1.py
@@ -0,0 +1,25 @@
+fname = input("Enter first name: ")
+lname = input("Enter last name: ")
+mname = input("Enter middle name: ")
+bday = input("Enter birthday: ")
+gender = input("Enter gender: ")
+address = input("Enter address: ")
+course = input("Enter course: ")
+ylevel = input("Enter year level: ")
+
+print("---------------------------------")
+
+print("First name: ", fname)
+print("Last name: ", lname)
+print("Middle name: ", mname)
+print("Birthday: ", bday)
+print("Gender: ", gender)
+print("Address: ", address)
+print("Course: ", course)
+print("Year level: ", ylevel)
+
+
+
+
+
+
