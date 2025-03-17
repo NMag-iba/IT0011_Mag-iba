71baa825c5baf7416bdb6257053a5e8d9b7f0e4e
NMag-iba <nhicamagiba2704@gmail.com>
2025-03-17 19:43:34 +0800

    17/03/2025

--- TW23/PE1/PE1.py
+++ TW23/PE1/PE1.py
@@ -1,13 +1,11 @@
-statement = """The quick brown fox jumps over the lazy dog near the bank of the river the Quick Brown Fox Jumps Over The Lazy Dog Near The Bank Of The River The quick brown fox jumps over the lazy dog near the bank of the river the Quick Brown Fox Jumps Over The Lazy Dog Near The Top Of The River The slow Brown fox jumps over the lazy dog near the bank of the river the Fast Brown Fox Jumps Over The Lazy Cat near The Bank Of The River The quick brown fox jumps over the Good Cat near the bank of the river the Quick Brown Fox Jumps Over The Lazy Dog Near The Bank Of The Mountain"""
+statement = input("Enter a string statement:\n")
 excluded_words = {"and", "but", "or", "nor", "for", "so", "yet", "a", "an", "the", "of"}
-
 words = statement.split()
-
 word_count = {}
 
 for word in words:
-    word = word.strip(".,!?")  
-    if word.lower() not in excluded_words:  
+    word = word.strip(".,!?")
+    if word.lower() not in excluded_words:
         if word in word_count:
             word_count[word] += 1
         else:
@@ -25,7 +23,7 @@ for word, count in word_count.items():
 n = len(lowercase_words)
 for i in range(n - 1):
     for j in range(0, n - i - 1):
-        if lowercase_words[j][0] > lowercase_words[j + 1][0]:  
+        if lowercase_words[j][0] > lowercase_words[j + 1][0]:
             temp = lowercase_words[j]
             lowercase_words[j] = lowercase_words[j + 1]
             lowercase_words[j + 1] = temp
@@ -33,7 +31,7 @@ for i in range(n - 1):
 n = len(uppercase_words)
 for i in range(n - 1):
     for j in range(0, n - i - 1):
-        if uppercase_words[j][0] > uppercase_words[j + 1][0]:  
+        if uppercase_words[j][0] > uppercase_words[j + 1][0]:
             temp = uppercase_words[j]
             uppercase_words[j] = uppercase_words[j + 1]
             uppercase_words[j + 1] = temp
